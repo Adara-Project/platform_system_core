master-hax/aosp-system-core

init: handle property service callbacks asynchronously
A previous change moved property_service  into its own thread, since
there was otherwise a deadlock whenever a process called by init would
try to set a property.  This new thread, however, would send a message
via a blocking socket to init for each property that it received,
since init may need to take action depending on which property it is.
Unfortunately, this means that the deadlock is still possible, the
only difference is the socket's buffer must be filled before init deadlocks.

There are possible partial solutions here: the socket's buffer may be
increased or property_service may only send messages for the
properties that init will take action on, however all of these
solutions still lead to eventual deadlock.  The only complete solution
is to handle these messages asynchronously.

This change, therefore, adds the following:
1) A lock for instructing init to reboot
2) A lock for waiting on properties
3) A lock for queueing new properties
4) A lock for any actions with ServiceList or any Services, enforced
   through thread annotations, particularly since this code was not
   designed with the intention of being multi-threaded.

Bug: 146877356
Bug: 148236233
Test: boot
Test: kill hwservicemanager without deadlock
Change-Id: I84108e54217866205a48c45e8b59355012c32ea8

init/init.h
@@ -41,6 +41,9 @@ void SendLoadPersistentPropertiesMessage();
 void SendStopSendingMessagesMessage();
 void SendStartSendingMessagesMessage();
 
+void property_changed(const std::string& name, const std::string& value);
+bool HandleControlMessage(const std::string& msg, const std::string& name, pid_t pid);
+
 int SecondStageMain(int argc, char** argv);
 
 }  // namespace init

Adara-Project/platform_system_core

A previous change moved property_service into its own thread, since
Merged-In: I84108e54217866205a48c45e8b59355012c32ea8
(cherry picked from commit 7205c6293341c82701e849fa29cfab66916d1052)

init/init.h
@@ -41,6 +41,9 @@ void SendLoadPersistentPropertiesMessage();
 void SendStopSendingMessagesMessage();
 void SendStartSendingMessagesMessage();
 
+void PropertyChanged(const std::string& name, const std::string& value);
+bool HandleControlMessage(const std::string& msg, const std::string& name, pid_t from_pid);
+
 int SecondStageMain(int argc, char** argv);
 
 }  // namespace init